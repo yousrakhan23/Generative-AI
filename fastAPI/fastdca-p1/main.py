f76e8ae96bdc244b876e3a9ef165dc01ee173a73
yousrakhan23 <ammarahkhan2016@gmail.com>
2025-05-07 19:29:34 +0500

    day 2

---
+++ fastAPI/fastdca-p1/main.py
@@ -0,0 +1,19 @@
+# def main():
+#     print("Hello from fastdca-p1!")
+
+
+# if __name__ == "__main__":
+#     main()
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str | None = None):
+    return {"item_id": item_id, "q": q}
